d47d22837b1ad1340a6090ab5aee8ef8958d45df
jon-zawada <jonathan_zawada@yahoo.com>
2021-07-04 01:42:37 -0700

    adds initial commit for point manager application

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <h2>Welcome to Jangos Coaching!</h2>
-      <h3>Date: { new Date().toDateString() }</h3>
+      <h2>Welcome to Point Manager!</h2>
     </div>
   );
 }
 
 export default App;
+
+
+
+/*
+ <Router>
+        <div>
+          <Header changeHover={this.changeHover} hover={hover} />
+          <Switch>
+            <Route path="/" exact render={() => <About changeHover={this.changeHover} />} />
+            <Route path="/about" component={() => <About changeHover={this.changeHover} />} />
+            <Route path="/projects" exact component={Projects} />
+            <Route path="/projects/:id" component={ProjectInfo} />
+          </Switch>
+        </div>
+      </Router>
+*/
